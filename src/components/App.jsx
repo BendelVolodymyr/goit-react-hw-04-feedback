45882c937552dc74c5383128e0755d3bf875d55a
BendelVolodymyr <bendel.vova@gmail.com>
2023-12-16 19:30:32 +0100

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,46 @@
-import React, {Component} from "react";
-import Statistics from "./Statistics/Statistics";
-import Section from "./Section/Section";
-import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+import { useState } from 'react';
+import Statistics from './Statistics/Statistics';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const allClick = good + neutral + bad;
+  const feedbackOptions = ['good', 'bad', 'neutral'];
+  const positivePercentage = Math.round((good * 100) / allClick);
 
-export class App extends Component {
-
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+  const handleLeaveFeedback = event => {
+    switch (event.target.textContent) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  click = event => {
-    this.setState({
-      [event]: this.state[event] + 1
-    });
-
-  };
-
-  allClick = ({good, neutral, bad}) => good + neutral + bad;
-
-  positivePercentage = ({ good, neutral, bad }) => Math.round((good * 100) / this.allClick(this.state)); 
-  
-  render() {
-    const { good, neutral, bad } = this.state; 
- return   (
+  return (
     <>
-      <Section title='Please leave feedback' >
-       <FeedbackOptions option={Object.keys(this.state)} onLeaveFeedback={this.click} />
-       <Statistics
-         good={good}
-         neutral={neutral}
-         bad={bad}
-         total={this.allClick(this.state)}
-         positivePercentage={this.positivePercentage(this.state)}
-       />
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          option={feedbackOptions}
+          onLeaveFeedback={handleLeaveFeedback}
+        />
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={allClick}
+          positivePercentage={positivePercentage}
+        />
       </Section>
     </>
   );
-};
-};
+}
